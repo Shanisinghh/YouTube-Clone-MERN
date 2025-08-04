58b5ac17c9072dee38e2797801c61ae24fec1506
shani <shanikushwaha8121@gmail.com>
2025-08-04 14:29:25 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://you-tube-clone-mern-gilt.vercel.app/",
+    "https://you-tube-clone-mern-gilt.vercel.app",
   ],
   credentials: true,
 }));
